6abc797a3e39909ea510f2232563ee3f6b9a988e
mbawon <jlucky046@gmail.com>
2021-12-15 17:05:31 +0000

    bug fix

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -12,7 +12,7 @@ const Signup = () => {
     const handleSubmit = async (e) =>{
         e.preventDefault()
         try{
-            const newSignUp = await authenticationService.signUp(signupInfo)
+            await authenticationService.signUp(signupInfo)
             setSignedup(!signedUp)
 
         }catch(error){
