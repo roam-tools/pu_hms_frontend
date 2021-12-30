27c41b3ee1489cab00810edb25421ed32e46eb62
mbawon <jlucky046@gmail.com>
2021-12-30 07:55:24 +0000

    auth fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import React from 'react';
-import {BrowserRouter as Router,Routes,Route, Navigate,useLocation} from 'react-router-dom'
-import { selectUser } from './features/authentication';
-import { useSelector } from 'react-redux';
-import DefaultLayout from './components/layout/DefaultLayout';
-import AdminLayout from './components/layout/AdminLayout';
-
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import { selectUser } from "./features/authentication";
+import { useSelector } from "react-redux";
+import DefaultLayout from "./components/layout/DefaultLayout";
+import AdminLayout from "./components/layout/AdminLayout";
 
 function App(props) {
   return (
     <Router>
       <Routes>
-        <Route path="/*" element={<DefaultLayout {...props}/>}/>
-        <Route path="/admin/*" element={<RequireAuth><AdminLayout {...props}/></RequireAuth>}/> />
+        <Route path="/*" element={<DefaultLayout {...props} />} />
+        <Route
+          path="/admin/*"
+          element={
+            <RequireAuth>
+              <AdminLayout {...props} />
+            </RequireAuth>
+          }
+        />{" "}
+        />
       </Routes>
     </Router>
   );
 }
 
-
 function RequireAuth({ children }: { children: JSX.Element }) {
-  let auth = useSelector(selectUser)
+  let auth = useSelector(selectUser);
   let location = useLocation();
 
-  if (auth.role === "student") {
+  if (
+    (!auth) ||
+    (auth.role !== "admin") ||
+    (auth.role !== "porter")
+  ) {
     return <Navigate to="/sign-in" state={{ from: location }} replace />;
   }
-  
+
   return children;
-
-
-
 }
 
 export default App;
